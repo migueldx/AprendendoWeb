2d0a1104a8699e636afb6a2a28ebcb888d7d8744
MIGUEL DIAS XIMENES <migueldiasximenes@gmail.com>
2017-11-28 01:00:25 -0300

    new controller

--- src/main/java/br/com/aprendendoweb/controller/UsuarioController.java
+++ src/main/java/br/com/aprendendoweb/controller/UsuarioController.java
@@ -1,4 +1,15 @@
 package br.com.aprendendoweb.controller;
 
+import br.com.aprendendoweb.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("usuariocontroller")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
 }
